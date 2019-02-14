6444a0abfdb5d998109c0e788946f55e026bcf07
Rafal Poltorak <peres09@gmail.com>
2019-02-14 21:51:38 +0100

    Added gitignore file and new file

--- statistics.sh
+++ statistics.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Skrypt tworzy plik z danymi na temat uzycia cpu (uzyte narzedzie sar)
-sar 5 100 >> statistics.txt 2>/dev/null 
+# Skrypt uruchamia sie w tle
+
+
+sar 5 100 >> statistics.txt 2>/dev/null &
